16ea32a3e29af0890cffd93104a35b2a7536e7e7
chris20-ui <christianbariasaguilar@gmail.com>
2026-05-28 20:54:04 +0800

    Add files via upload

---
+++ tempt_day7.py
@@ -0,0 +1,11 @@
+temperature =45
+
+if temperature>=35:{
+    print("it's really hot in phillipines")
+}
+elif temperature>=30:{
+    print("it's normal temperature")
+}    
+else:{
+    print("it's cold today")
+}

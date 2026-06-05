77088ad833d407bcd35109492cbe9a73284dd8df
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-05 18:34:06 +0800

    Add files via upload

---
+++ temp2028.py
@@ -0,0 +1,13 @@
+Temperature = 50
+
+if Temperature >=45:{
+    print("it's really hot here in the philippines")
+}
+    
+elif Temperature >=25:{
+    print("finally a normal weather")
+}
+    
+else:{
+    print("its so cold")
+}

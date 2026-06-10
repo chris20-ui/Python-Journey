7aa930bc3f602696e184ce729f1418d0bc7f0d51
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-10 20:50:59 +0800

    Add files via upload

---
+++ simple_temperature_checker.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    Temperature = int(input("Enter Temperature: "))
+    if Temperature>=40:
+        print("it's very hot")
+    elif Temperature>=25:
+        print("lovely weather")
+    else:
+        print("its very cold")
---
+++ temp_app.py
@@ -0,0 +1,8 @@
+for i in range(1):
+    Temperature=int(input("Enter Tempearture: "))
+    if Temperature>=40:
+        print("it's fucking hot")
+    elif Temperature>=25:
+        print("Finally a relaxing weather")
+    else:
+        print("coldddddd")

f27990c8760e972294afeb8b4b92d37eff4a5adf
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-11 13:17:38 +0800

    Add files via upload

---
+++ Grade_Checker.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    Grade =int(input("Enter Your Grade: "))
+    if Grade>=90:
+        print("Outstanding")
+    elif Grade>=70:
+        print("You Passed")
+    else:
+        print("Fail")
---
+++ Temperature_Checker.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    Temperature=int(input("Enter Temperature: "))
+    if Temperature>=45:
+        print("it's vert hot")
+    elif Temperature>=25:
+        print("relaxing weather")
+    else:
+        print("it's so cold...")

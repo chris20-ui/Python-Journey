8dd1f410bcc46db6dc6dd3fe15c4d3229f273c15
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-12 11:29:15 +0800

    Add files via upload

---
+++ GWA_checker.py
@@ -0,0 +1,12 @@
+for i in range(4):
+    Grade = int(input("Enter Your'e GWA "))
+    if Grade>=99:
+        print("Congrats You're a Summa Cum Laude")
+    elif Grade>=95:
+        print(" Congrats You're a Magna Cum Laude")
+    elif Grade>=91:
+        print("Congrats you're a Cum Laude")
+    else:
+        print("Congrats You Graduate ")
+
+    
--- Temperature_Checker.py
+++ Temperature_Checker.py
@@ -1,8 +1,8 @@
 for i in range(3):
-    Temperature=int(input("Enter Temperature: "))
+    Temperature=int(input("Enter Today's Temperature: "))
     if Temperature>=45:
-        print("it's vert hot")
+        print("it's very hot today")
     elif Temperature>=25:
-        print("relaxing weather")
+        print("finally a relaxing weather")
     else:
-        print("it's so cold...")
+        print("it's so cold")

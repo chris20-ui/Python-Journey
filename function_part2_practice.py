faa54e6f5e66890b9d318901e5feef717d81203b
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-14 13:29:00 +0800

    Add files via upload

---
+++ function_part2_practice.py
@@ -0,0 +1,5 @@
+def good_afternoon(name):
+    print("how are you?",name)
+
+
+good_afternoon("chris")
---
+++ function_part3.py
@@ -0,0 +1,6 @@
+def meow(name):
+    print("how's your day?" , name)
+   
+meow("chris")
+meow("ada")
+meow("claire")

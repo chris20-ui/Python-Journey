243eba64f01b596e8978b197d393ed3405839191
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-16 20:52:31 +0800

    Add files via upload

---
+++ 3rd_practive.py
@@ -0,0 +1,8 @@
+for i in range(4):
+   Temp = int (input("Enter Temp Today: "))
+   if Temp>=50:
+    print("It's fucking hot today")
+   elif Temp>=25:
+    print("yeyy relaxing weather ehehhehe")
+   else:
+    print("it's so cold")
---
+++ Calendar_meow.py
@@ -0,0 +1,5 @@
+import calendar
+
+year=2026
+month=5
+print(calendar.month(year,month))

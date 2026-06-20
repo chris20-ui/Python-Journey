6431082461de1bdf34dbc0c7eaae0ac4d5881a85
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-20 09:45:21 +0800

    Add files via upload

---
+++ practicw_4.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    Temp = int(input("Enter Today Temperature: "))
+    if Temp>=50:
+        print("It's fucking hot today") 
+    elif Temp>=25:
+       print("Finally a relaxing weather")
+    else:
+       print("it's very cold") 

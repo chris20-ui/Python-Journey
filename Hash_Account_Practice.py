25eb616bb7e03caa3ff7b9187645bfeabe5f5695
chris20-ui <christianbariasaguilar@gmail.com>
2026-06-21 17:25:48 +0800

    Add files via upload

---
+++ Hash_Account_Practice.py
@@ -0,0 +1,6 @@
+username = input ("Create You're Username : ")
+password = input ("Create You're Password : ")
+
+print("\n Account has been Created")
+print("Username: ",username)
+print("Password: ",password)

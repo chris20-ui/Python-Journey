bf86724703879eeb3c9b4b91aa3b6369f581645d
chris20-ui <christianbariasaguilar@gmail.com>
2026-07-09 12:57:25 +0800

    Add files via upload

---
+++ july9_part2.py
@@ -0,0 +1,16 @@
+for i in range (5):
+
+    Grade = float(input("Enter you're Grade: "))
+
+    if Grade>=99:
+        print("You're an Achiever ")
+
+    elif Grade>=95:
+        print("Wow you still an Achiever")
+
+    elif Grade>= 60:
+        print("You Passed ")
+
+
+    else:
+        print("repeat")

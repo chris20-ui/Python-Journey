171a6950c9bfae221f27203d62b48e9aad669e60
chris20-ui <christianbariasaguilar@gmail.com>
2026-07-12 17:01:32 +0800

    Add files via upload

---
+++ july12_p3.py
@@ -0,0 +1,12 @@
+for i in range(3):
+
+    Grade = float(input("Enter Grade: "))
+
+    if Grade >=99:
+        print("Achiever")
+
+    elif Grade>=60:
+        print("Passed")
+
+    else: 
+        print("Repeater")

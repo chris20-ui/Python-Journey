aa5f232cb03697295a0437434c7c721aa0a5680c
chris20-ui <christianbariasaguilar@gmail.com>
2026-07-20 19:42:08 +0800

    Add files via upload

---
+++ july20p3.py
@@ -0,0 +1,16 @@
+import bcrypt
+
+username = input("Create Username: ")
+
+password = input("Create Password: ")
+
+hashed_password = bcrypt.hashpw(
+    password.encode("utf-8"),
+    bcrypt.gensalt()
+)
+
+print("\n Account Created Succesful")
+
+print("Username: ",username)
+
+print("Password",hashed_password)

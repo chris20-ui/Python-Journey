14eddd6f6d8ee1fa36e68d2d13ce1c57069d5963
chris20-ui <christianbariasaguilar@gmail.com>
2026-07-29 13:01:57 +0800

    Add files via upload

---
+++ Temperature_july29.py
@@ -0,0 +1,12 @@
+for i in range(4):
+   Temperature = int (input("Enter Temperature: "))
+   if Temperature>=50:
+            print("Very Hot")
+   elif Temperature>=35:
+            print("Hot")
+
+   elif Temperature>=25:
+            print("Normal Weather")
+
+   else:
+            print("Cold")

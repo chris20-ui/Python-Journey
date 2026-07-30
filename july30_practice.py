7a658c151fac95c634a092b0dff5dfcef4ccdb8c
chris20-ui <christianbariasaguilar@gmail.com>
2026-07-30 17:07:00 +0800

    Add files via upload

---
+++ july30_practice.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    number = int (input("Enter a number: "))
+
+    if number>0:
+        print("Positive: ",i)
+    elif number<0:
+        print("Negative: ",i)
+
+    else:
+        print("Zero: ",i)

8e64d5fb548d9bddbf8a5d0b6ac3e90872d87864
chris20-ui <christianbariasaguilar@gmail.com>
2026-07-31 10:55:26 +0800

    Add files via upload

---
+++ july31_entry2.py
@@ -0,0 +1,25 @@
+gamecard = 0
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=1
+
+gamecard +=10
+
+print(f"i gain {gamecard} after watching ads")

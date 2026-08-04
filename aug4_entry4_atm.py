dd00571f5a15a6b8d568bc1e18873acb74195588
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-04 11:38:37 +0800

    Add files via upload

---
+++ aug4_entry4_atm.py
@@ -0,0 +1,21 @@
+Balance = 1000
+
+Deposit = 0 
+
+
+print(f"Current Balance: {Balance}")
+
+
+Deposit = int(input("Enter the Amount to Deposit: "))
+
+Balance+=Deposit
+
+print(f"New Balance: {Balance} ")
+
+
+
+
+
+
+
+

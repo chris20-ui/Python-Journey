4b1dac215e543195d6906cb7ed88eaac8baf39dd
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-05 22:05:31 +0800

    Add files via upload

---
+++ Mana_Potion_Aug5.py
@@ -0,0 +1,12 @@
+Mana = 100
+ 
+Potion = 0
+
+print (f" Current Mana: {Mana}")
+
+
+Potion = int(input("Enter the amount of potion that would you like to drink: "))
+
+Mana +=Potion
+
+print(f"New Mana: {Mana}")

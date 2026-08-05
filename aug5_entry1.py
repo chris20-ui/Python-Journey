b6e102a66fd452038f69a377c0904d5a6da12418
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-05 12:53:05 +0800

    Add files via upload

---
+++ aug5_entry1.py
@@ -0,0 +1,14 @@
+shadow_army = 1
+
+enemy_defeated = 0
+
+
+print(f"Current Shadow Army: {shadow_army} ")
+
+enemy_defeated = int(input("Enter how many have you been defeated: "))
+
+shadow_army+=enemy_defeated
+
+
+
+print(f"Total Shadow Army: {shadow_army} ")

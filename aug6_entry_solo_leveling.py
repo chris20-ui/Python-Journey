89bd6021b4a0261768e07c35a6806e9189d594cf
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-06 12:14:19 +0800

    Add files via upload

---
+++ aug6_entry_solo_leveling.py
@@ -0,0 +1,11 @@
+shadow = 50
+extract_shadow = 0
+
+print(f"Current Shadow Army: {shadow}")
+
+
+extract_shadow = int(input("Enter How manny shadow have you extracted? "))
+
+shadow += extract_shadow
+
+print(f" Congats You have now extracted {shadow}!")

e31c25ae9495a69849668f89d0d24c5cd12cb378
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-06 17:14:56 +0800

    Add files via upload

---
+++ Aug6Campfire_rest.py
@@ -0,0 +1,15 @@
+Health = 50
+
+Recover = 0
+
+
+print(f"Current Health: {Health} ")
+
+
+
+
+Recover = int(input("Enter how much did you recover: "))
+
+Health += Recover 
+
+print(f" New Hp: {Health} ")

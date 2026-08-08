e26c63cfbdf632126403de08b61992c63d9577db
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-08 19:28:50 +0800

    Add files via upload

---
+++ Aug8_Rpg_Potion_Shop.py
@@ -0,0 +1,14 @@
+Gold = 500
+
+Potion_Price = 50
+
+Potion = 0
+
+
+print(f" Current {Gold}")
+
+Potion = int(input("How many potions did you buy? "))
+
+Gold -= Potion_Price * Potion
+
+print(f"Remaining Gold: {Gold}")

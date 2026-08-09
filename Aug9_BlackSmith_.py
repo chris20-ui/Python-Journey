91fa6d9d675c1893fc8d68dfde70e4f62bb61b62
chris20-ui <christianbariasaguilar@gmail.com>
2026-08-09 21:01:19 +0800

    Add files via upload

---
+++ Aug9_BlackSmith_.py
@@ -0,0 +1,15 @@
+Gold = 10000
+
+quantity = 0
+
+price = 500
+
+
+print(f"Current {Gold}")
+
+quantity = int(input("Enter the amount: "))
+
+
+Gold -= price*quantity
+
+print(f"Remaining Gold: {Gold}")

8ba99363db4cffefa9b428047dc93032f5b25c20
chris20-ui <christianbariasaguilar@gmail.com>
2026-09-10 22:16:54 +0800

    Add files via upload

---
+++ Sept9_entry3.py
@@ -0,0 +1,21 @@
+gold = 2000
+
+quantity = 0
+
+
+price = 500
+
+bonus = 200
+
+
+print(f"Currnet Gold: {gold}")
+
+quantity = int(input("Enter how many upgrade would you like to do? "))
+
+
+if quantity >=4:
+    gold -= price * quantity
+    gold += bonus * quantity
+
+    print(f"Remaining Gold: {gold}")
+
